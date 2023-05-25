e95d1c3046aa9309af909f25d0cd2b960a1d71a1
JM-Na <115651185+JM-Na@users.noreply.github.com>
2023-05-25 23:55:10 +0900

    --

--- src/main/java/com/gachon/springtermproject/entity/Event.java
+++ src/main/java/com/gachon/springtermproject/entity/Event.java
@@ -7,9 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Setter
--- src/main/java/com/gachon/springtermproject/repository/TournamentRepository.java
+++ src/main/java/com/gachon/springtermproject/repository/TournamentRepository.java
@@ -1,11 +1,9 @@
 package com.gachon.springtermproject.repository;
 
 import com.gachon.springtermproject.entity.Tournament;
-import org.json.simple.JSONObject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Map;
 
 @Repository
 public interface TournamentRepository extends JpaRepository<Tournament, Long>{
